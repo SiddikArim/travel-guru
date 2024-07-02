b978fb7d7fe42fea4f5a8528f8d2b42fa4126b52
Siddik Arim <arim99999@gmail.com>
2024-07-02 19:32:14 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Footer from "./components/sharedComponents/Footer";
+import Navbar from "./components/sharedComponents/Navbar";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
